b64692ed37d25489d1242d623d3e0709994c7ed1
sureshkumarks1 <sureshkumarks1@gmail.com>
2025-11-18 14:22:07 +0530

    added backend

--- back/index.js
+++ back/index.js
@@ -1,18 +1,35 @@
-const express = require('express');
-const feedbackRoutes = require('./router/feedbackroute');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const feedbackRoute = require("./routes/feedbackRoute");
+const app = express();
 
-require('dotenv').config();
-const app= express();
-const PORT= process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+app.use(
+  cors({
+    origin: "https://feedback-front-qftd.onrender.com/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(express.json());
+if (!MONGO_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected Successfully");
+  })
+  .catch((err) => console.error(err));
 
-app.get("/",(req,res)=>{
-    res.send("API is running...");
+app.use("/api/feedback", feedbackRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/feedback', feedbackRoutes);
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
--- back/router/feedbackroute.js
+++ back/router/feedbackroute.js
@@ -1,8 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Feedback = require("../models/Feedback");
 
-router.get('/', (req, res) => {
-  res.send("Feedback route working");
+/**
+ * This route is created for adding new feedback from frontend
+ *
+ */
+router.post("/submit", async (req, res) => {
+  try {
+    const newFeedback = new Feedback(req.body);
+    const saved = await newFeedback.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
+
+//getting all submitted feedbacks
+router.get("/all", async (req, res) => {
+  try {
+    const all = await Feedback.find().sort({ createdAt: -1 });
+    res.json(all);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+/**
+ * this route is created for deleting feedback
+ * argument feedback ID is required for deleting feedback from database
+ * return true if the feedback is deleted
+ */
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+  
+    const result = await Feedback.findByIdAndDelete(id); 
+    if (!result) {
+      return res.json({ success: false, message:"Feedback Not Found"});   }   
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put('/update/:id',async (req, res)=>{
+   const { id } = req.params;
+  try{
+
+    const update = await Feedback.findByIdAndUpdate(id, req.body, {new:true});
+    res.json(update)
+  }catch(error){
+    res.status(500).json({ error: error.message });
+  }
+})
+
 
 module.exports = router;
